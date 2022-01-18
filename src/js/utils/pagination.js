2acdc633e467eecd5d0c442fe337f1fd330b9640
Miguelidze <migueldeorpesa@gmail.com>
2022-01-19 00:54:08 +0100

    start scss

--- src/js/utils/pagination.js
+++ src/js/utils/pagination.js
@@ -1,15 +1,12 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
-const BASE_URL = `https://api.themoviedb.org/3`;
-const API_KEY = `eba0388c934688725105b53c98cf82ca`;
-
 const options = { 
-  totalItems: 20,
+  totalItems: 1000,
   itemsPerPage: 20,
-  visiblePages: 50,
+  visiblePages: 5,
   page: 1,
-  centerAlign: false,
+  centerAlign: true,
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
@@ -31,14 +28,14 @@ console.dir(pagination);
 
 const page = pagination.getCurrentPage()
 
-fetchMoviesCard(page).then(({ images, totalItems }) => {pagination.reset(totalItems);renderImages(images)})
-async function fetchMoviesCard(page) {
-    const res = await fetch(`${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}&per_page=10`);
-  const data = res.json;
-  return ({ images: data.hits, total: data.totalHits });
-}
+// fetchMoviesCard(page).then(({ images, totalItems }) => {pagination.reset(totalItems);renderImages(images)})
+// async function fetchMoviesCard(page) {
+//     const res = await fetch(`${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}&per_page=10`);
+//   const data = res.json;
+//   return ({ images: data.hits, total: data.totalHits });
+// }
 
-function renderImages (images) {
-  // console.log(images);
-  console.log(`render`);
-}
+// function renderImages (images) {
+//   // console.log(images);
+//   console.log(`render`);
+// }
